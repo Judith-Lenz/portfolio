9d0def5b6326f2575acd26ad926f95b462648178
Judith L. <judith.lenz@outlook.de>
2025-03-25 15:46:20 +0100

    scrolltotop delete path

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-title',
@@ -9,12 +10,13 @@ import { RouterModule, Router } from '@angular/router';
   styleUrl: './title.component.scss',
 })
 export class TitleComponent {
-  constructor(private router: Router) {}
+  constructor(private location: Location, private router: Router) {}
   navigateToHome() {
     const path = this.router.url.split('#')[0];
     if (path === '/' || path === '') {
       // Schon auf der Hauptseite → smooth scroll nach oben
       window.scrollTo({ top: 0, behavior: 'smooth' });
+      this.location.replaceState('/');
     } else {
       // Auf einer anderen Seite → navigieren und ohne smooth scrollen
       this.router.navigate(['/']).then(() => {
